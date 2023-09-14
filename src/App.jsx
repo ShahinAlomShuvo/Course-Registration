769d717b5fb32a9f78c5ba8c6e098679a4505efd
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-09-14 22:43:41 +0600

    make display card responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <h1 className='text-center text-3xl font-bold py-10'>
         Course Registration
       </h1>
-      <div className='grid grid-cols-4 gap-5 container mx-auto'>
+      <div className='grid grid-cols-4 gap-5 container mx-auto px-3 md:px-0'>
         <Courses></Courses>
         <Cart></Cart>
       </div>
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -5,15 +5,17 @@ const Course = ({ course }) => {
   const { course_title, course_banner, course_details, price, credit } = course;
   return (
     <>
-      <div className=''>
+      <div>
         <div className='card  bg-base-100 shadow-xl'>
-          <figure className='px-4 pt-5'>
+          <figure className='px-2 md:px-4 pt-5'>
             <img src={course_banner} className='rounded-xl' />
           </figure>
-          <div className='card-body px-4'>
-            <h2 className='card-title text-[18px]'>{course_title}</h2>
+          <div className='card-body px-2 md:px-4'>
+            <h2 className='card-title text-base xl:text-[18px]'>
+              {course_title}
+            </h2>
             <p className='text-sm text-gray-500 '>{course_details}</p>
-            <div className='flex justify-between py-2'>
+            <div className='flex flex-col gap-2 xl:flex-row justify-between py-2'>
               <div className='flex items-center gap-2 text-gray-600'>
                 <FontAwesomeIcon className='text-2xl' icon={faDollarSign} />
                 <span>Price: {price} </span>
