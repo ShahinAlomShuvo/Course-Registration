fdb40e12ca58d0af176c5897b75894bebfb596c2
Shahin Alom <shahinalomshuvo94@gmail.com>
2023-09-15 13:50:19 +0600

    add remaining & total functionality & handle error notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
 import { useState } from "react";
 import Cart from "./components/Cart";
 import Courses from "./components/Courses";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 
 const App = () => {
   const [addCourse, setAddCourse] = useState([]);
   const [credit, setCredit] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [remainingCredit, setRemainingCredit] = useState(20);
 
   const cartHandler = (course) => {
     const isExits = addCourse.find((exitCourse) => exitCourse.id === course.id);
     if (isExits) {
-      return alert("You Already Purchase This Course");
+      return toast.warning("You Already Select This Course");
     }
 
+    if (remainingCredit - course.credit < 0) {
+      return toast.error("Insufficient Credit");
+    }
+    if (credit + course.credit > 20) {
+      return toast.error("You Over Your Credit");
+    }
+    setRemainingCredit(remainingCredit - course.credit);
     setCredit(credit + course.credit);
+    setPrice(price + course.price);
     setAddCourse([...addCourse, course]);
   };
   return (
     <>
-      <h1 className='text-center text-3xl font-bold py-10'>
-        Course Registration
-      </h1>
+      <Header></Header>
       <div className='grid grid-cols-4 gap-5 container mx-auto px-3 md:px-0'>
         <Courses cartHandler={cartHandler}></Courses>
-        <Cart addCourse={addCourse} credit={credit}></Cart>
+        <Cart
+          addCourse={addCourse}
+          remainingCredit={remainingCredit}
+          credit={credit}
+          price={price}
+        ></Cart>
       </div>
+      <ToastContainer position='top-center' autoClose={5000} />
     </>
   );
 };
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,12 @@
 import CartItem from "./CartItem";
 
-const Cart = ({ addCourse, credit }) => {
+const Cart = ({ addCourse, credit, price, remainingCredit }) => {
   return (
     <>
-      <div className='col-span-2 md:col-span-1  '>
-        <div className='card  bg-base-100 shadow-xl px-2 py-4'>
+      <div className='col-span-2 md:col-span-1 '>
+        <div className='card  bg-base-100 shadow-xl px-5   py-4'>
           <div className='py-4 border-b-2 text-[18px] text-blue-500 font-bold'>
-            <h2>Credit Hour Remaining 7 hr</h2>
+            <h2>Credit Hour Remaining {remainingCredit} hr</h2>
           </div>
           <h2 className='text-[18px] font-bold pt-4'>Course Name</h2>
           <div className='py-5  border-b-2'>
@@ -14,7 +14,10 @@ const Cart = ({ addCourse, credit }) => {
               <CartItem key={title.id} title={title} idx={idx}></CartItem>
             ))}
           </div>
-          <h2>Total Credit Hour : {credit}</h2>
+          <h2 className='font-medium py-3 border-b-2'>
+            Total Credit Hour : {credit}
+          </h2>
+          <h3 className='font-medium py-3 '>Total Price : {price} USD</h3>
         </div>
       </div>
     </>
